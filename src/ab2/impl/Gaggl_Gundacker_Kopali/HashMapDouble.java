aa40d1110188d294d9c7ddc16fd094428fe4b283
Felix Gaggl <felix.gaggl@outlook.com>
2018-06-02 15:31:28 +0200

    Commented HashMapLinear and HashMapDouble

--- src/ab2/impl/Gaggl_Gundacker_Kopali/HashMapDouble.java
+++ src/ab2/impl/Gaggl_Gundacker_Kopali/HashMapDouble.java
@@ -10,10 +10,12 @@ public class HashMapDouble extends AbstractHashMap {
     private int elements;
 
     public HashMapDouble(int minSize) {
-        int prim = getPrim(minSize);
+        int prim = getPrim(minSize);            //bei der initialisierung der Hashmap muss überprüft werden ob die größe des tables eine primzahl ist
         initTable(prim);
     }
-
+    /*
+    * Hilfsmethode, die die nächst kleinste primzahl von der eingegeben findet, wenn diese zahl bereits eine primzahl ist wird sie nicht verändert
+    */
     private int getPrim(int a) {
         int i = a;
         while (!checkPrim(i)) {
@@ -22,6 +24,9 @@ public class HashMapDouble extends AbstractHashMap {
         return i;
     }
 
+    /*
+    * Hilfsmethode, die überprüft ob eine Zahl eine Primzahl ist oder nicht
+    */
     private boolean checkPrim(int a) {
         if(a%2==0 || a%3==0 || a%5==0 ){
             return false;
@@ -32,53 +37,57 @@ public class HashMapDouble extends AbstractHashMap {
     @Override
     public boolean put(int key, String value) {
 
-        //If HashMap is full, return false
-        if (elementCount() >= totalSize()) {
+
+        if (elementCount() >= totalSize()) {                    //wenn die Hashmap voll ist kann false zurückgegeben werden
             return false;
         }
 
-        int index = calcModuloIndex(key);
-        int offset = 0;
-        int offsetCount=1;
+        int index = calcModuloIndex(key);                       //berechne den Startindex
+        int offset = 0;                                         //der Startoffset = 0
+        int offsetCount=1;                                      //offsetCount steht für die anzahl der versuchten stellen (steht für j bei der Doppelhashing sondierung: j*(1+(h'(k)))
 
         do{
-            if(getValue(index + offset) == null){
+            if(getValue(index + offset) == null){           //wenn der wert an der stelle index+offset = null ist dann setze den wert an die stelle index+offset
                 setKeyAndValue(index+offset,key,value);
-                elements++;
+                elements++;                                     //inkrementiere den elements zähler
                 return true;
-            }else if(getKey(index + offset) == key){
+            }else if(getKey(index + offset) == key){        //wenn der wert bereits an der stelle vorkommt dann ist der Wert doppelt vorhanden
                 setKeyAndValue(index+offset, key, value);
                return true;
             }
 
-            offset = getSecondFunction(index,offsetCount);
-            offsetCount++;
+            offset = getSecondFunction(index,offsetCount);     //berechne den wert der zweiten hash-funktion
+            offsetCount++;                                     //erhöhe die anzahl der versuchten stellen
 
-        }while(index + offset != index);
+        }while(index + offset != index);                       //wiederhole diese schleife bis der berechnete index gleich dem startindex ist, sollte nicht bis dahin kommen, da jede freie stelle gefunden wird
 
         return false;
     }
-
+    /*
+    * Berechnet den Wert der zweiten Hashfunktion: j*(1 + k MOD(m − 2))*/
     private int getSecondFunction(int index,int offsetCount) {
         return offsetCount*(1+(index%(totalSize()-2)));
     }
 
+
+    /*
+    * Methode berechnet den "ersten" index an welchen der Wert gespeichert werden soll, (k MOD m)*/
     private int calcModuloIndex(int key) {
         return key % totalSize();
     }
 
     @Override
     public String get(int key) {
-       int index = calcModuloIndex(key);
-       int offset=0;
-       int offsetCount=1;
+       int index = calcModuloIndex(key);                //startindex wird berechnet
+       int offset=0;                                    //der offset ist am anfang = 0
+       int offsetCount=1;                               //offsetCount steht für die anzahl der versuchten stellen
 
-       while (getKey(index+offset)!=null){
-           if(getKey(index+offset)==key){
+       while (getKey(index+offset)!=null){          //wiederhole diese schleife bis der key an der stelle index+offset = null ist, dann wurde der wert nicht gefunden
+           if(getKey(index+offset)==key){           //wenn der key an der stelle index+offset gleich dem zu suchenden key ist dann gib den wert an der stelle zurück
                return getValue(index+offset);
            }
-           offset=getSecondFunction(index,offsetCount);
-           offsetCount++;
+           offset=getSecondFunction(index,offsetCount); //offset wird nach jedem schleifendurchlauf neu berechnet
+           offsetCount++;                               //anzahl der versuchten stellen wird erhöht, wichtig um im nächsten schleifendurchlauf einen anderen wert von der zweiten hashfunktion zu bekommen
        }
 
         return null;
--- src/ab2/impl/Gaggl_Gundacker_Kopali/HashMapLinear.java
+++ src/ab2/impl/Gaggl_Gundacker_Kopali/HashMapLinear.java
@@ -18,7 +18,7 @@ public class HashMapLinear extends AbstractHashMap {
     @Override
     public boolean put(int key, String value) {
 
-       //If HashMap is full, return false
+       //Wenn die Hashmap voll ist kann false zurückgegeben werden
         if(elementCount()>=totalSize()){
             return false;
         }
@@ -27,35 +27,36 @@ public class HashMapLinear extends AbstractHashMap {
         int idx = calcModuloIndex(key);         //erste "korrekte" position wird berechnet mit dem übergebenen key
         int offset=0;                           //am anfang ist der offset = 0
         do {
-            if(getValue(idx+offset) == null){
+            if(getValue(idx+offset) == null){                  //wenn der wert an der stelle idx+offset leer ist kann das element eingefügt werden
                 setKeyAndValue(idx + offset, key, value);
-                elements++;
+                elements++;                                        //erhöhe die anzahl der elemente
                 return true;
             }
-            if(getKey(idx + offset) == key){
+            if(getKey(idx + offset) == key){                    //wenn der key an der stelle idx+offset = dem zu speichernden wert ist, dann existiert der wert bereits
                 setKeyAndValue(idx + offset, key, value);
                 return true;
             }
-            offset--;
+            offset--;                                               //offset wird nach jedem while Schleifendurchlauf dekrementiert (lineare Sondierung)
 
-        }while(idx+offset != idx);
-        return false;
-        
+        }while(idx+offset != idx);                                  //führe die Schleife sooft aus, bis der Index wieder der Startindex ist, dazu sollte es im Normalfall nicht kommen,
+        return false;                                               //bereits oben abgefragt wird ob noch platz ist, wenn platz vorhanden ist, so sollte dieser auch gefunden werden
+
     }
-
+    /*
+    * Methode berechnet den "ersten" index an welchen der Wert gespeichert werden soll, (k MOD m)*/
     private int calcModuloIndex(int a){
         return a%totalSize();
     }
 
     @Override
     public String get(int key) {
-        int idx=calcModuloIndex(key);
-        int offset=0;
-        while(getKey(idx+offset)!=null){
-            if(getKey(idx+offset)==key){
+        int idx=calcModuloIndex(key);                               //Startindex wird berechnet
+        int offset=0;                                               //der offset ist am anfang = 0
+        while(getKey(idx+offset)!=null){                        //führe schleife aus bis der wert an der stelle idx+offset leer ist, dann wurde der wert nicht gefunden
+            if(getKey(idx+offset)==key){                        //wenn der key an der stelle idx+offset gleich dem gesuchten key ist dann gib den wert an der stelle idx+offset aus
                 return getValue(idx+offset);
             }
-            offset-=1;
+            offset-=1;                                              //nach jedem Schleifendurchlauf wird der Index verringert (lineare Sondierung)
         }
 
         return null;
